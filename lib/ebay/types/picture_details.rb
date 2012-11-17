27f1795fd8df41f1e715a5183d82128542c2b5fa
Adeel Ahmad <adeel@adeelahmad.com>
2012-11-17 00:29:19 -0800

    array for picture urls

--- lib/ebay/types/picture_details.rb
+++ lib/ebay/types/picture_details.rb
@@ -18,7 +18,7 @@ module Ebay # :nodoc:
       text_node :gallery_type, 'GalleryType', :optional => true
       text_node :gallery_url, 'GalleryURL', :optional => true
       text_node :photo_display, 'PhotoDisplay', :optional => true
-      text_node :picture_url, 'PictureURL', :optional => true
+      value_array_node :picture_urls, 'PictureURL', :default_value => []
       text_node :picture_source, 'PictureSource', :optional => true
       text_node :gallery_duration, 'GalleryDuration', :optional => true
       text_node :gallery_status, 'GalleryStatus', :optional => true
